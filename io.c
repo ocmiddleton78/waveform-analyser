1aa08f0807e71a59c5a47235a3e6db1f6d283cfb
Owen Middleton <owen2.middleton@live.uwe.ac.uk>
2026-05-02 14:49:15 +0100

    Started load function and opened file

--- io.c
+++ io.c
@@ -5,3 +5,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "io.h"
+
+WaveformSample *load_csv(const char *filename, size_t *count) {
+    FILE *file = fopen(filename, "r");
+
+    if (file == NULL) {
+        printf("Error opening file/n");
+        return NULL;
+    }
+
+    fclose(file);
+    return NULL;
+}
