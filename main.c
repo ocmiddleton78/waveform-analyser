61794f03de5993705d82141ef7ffdc17468f8a35
Owen Middleton <owen2.middleton@live.uwe.ac.uk>
2026-05-02 15:42:50 +0100

    Linked load_csv to main and added a file check

--- main.c
+++ main.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "io.h"
 
-int main() {
-    printf("Hello, World!\n");
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <input file>\n", argv[0]);
+        return 1;
+    }
+
+    size_t count = 0;
+
+    WaveformSample *data = load_csv(argv[1], &count);
+
+    if (data == NULL) {
+        printf("File could not be loaded\n");
+        return 1;
+    }
+
+    printf("File opened successfully\n");
+
+    free(data);
+
     return 0;
 }
+
