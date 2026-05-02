e81ad1fbb64e0d6124eae64b1af05a99c47fdefa
Owen Middleton <owen2.middleton@live.uwe.ac.uk>
2026-05-02 13:24:49 +0100

    Added waveform struct

--- waveform.h
+++ waveform.h
@@ -2,7 +2,21 @@
 // Created by owen on 17/04/2026.
 //
 
-#ifndef CODE_WAVEFORM_H
-#define CODE_WAVEFORM_H
+#ifndef WAVEFORM_H
+#define WAVEFORM_H
 
-#endif //CODE_WAVEFORM_H
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef struct {
+    double timestamp;
+    double phase_A_voltage;
+    double phase_B_voltage;
+    double phase_C_voltage;
+    double line_current;
+    double frequency;
+    double power_factor;
+    double thd_percent;
+} WaveformSample;
+
+#endif
