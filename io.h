19475ae68c96d614c09ad90454e31b588c2af2a1
Owen Middleton <owen2.middleton@live.uwe.ac.uk>
2026-05-02 14:27:14 +0100

    Added io header with file loading and results functions

--- io.h
+++ io.h
@@ -2,7 +2,18 @@
 // Created by owen on 17/04/2026.
 //
 
-#ifndef CODE_IO_H
-#define CODE_IO_H
+#ifndef IO_H
+#define IO_H
 
-#endif //CODE_IO_H
+#include <stddef.h>
+#include "waveform.h"
+
+WaveformSample *load_csv(const char *filename, size_t *count);
+
+int write_results(const char *filename,
+                  double rmsA , double rmsB , double rmsC ,
+                  double p2pA , double p2pB , double p2pC ,
+                  double dcA , double dcB , double dcC ,
+                  int clipA , int clipB , int clipC);
+
+#endif
