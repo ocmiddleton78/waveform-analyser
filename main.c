f616285e29b560487ff04c0de91fd1aed41b9ad5
Owen Middleton <owen2.middleton@live.uwe.ac.uk>
2026-05-07 13:23:55 +0100

    Removed debug prints

--- main.c
+++ main.c
@@ -22,41 +22,21 @@ int main(int argc, char *argv[]) {
     double dcB = calculate_dc_offset(data, count, 'B');
     double dcC = calculate_dc_offset(data, count, 'C');
 
-    printf("DC Offset A: %f\n", dcA);
-    printf("DC Offset B: %f\n", dcB);
-    printf("DC Offset C: %f\n", dcC);
-
     double rmsA = calculate_rms(data, count, 'A');
     double rmsB = calculate_rms(data, count, 'B');
     double rmsC = calculate_rms(data, count, 'C');
-
-    printf("RMS A: %f\n", rmsA);
-    printf("RMS B: %f\n", rmsB);
-    printf("RMS C: %f\n", rmsC);
 
     double p2pA = calculate_peak_to_peak(data, count, 'A');
     double p2pB = calculate_peak_to_peak(data, count, 'B');
     double p2pC = calculate_peak_to_peak(data, count, 'C');
 
-    printf("Peak to peak A: %f\n", p2pA);
-    printf("Peak to peak B: %f\n", p2pB);
-    printf("Peak to peak C: %f\n", p2pC);
-
     int clipA = detect_clipping(data, count, 'A');
     int clipB = detect_clipping(data, count, 'B');
     int clipC = detect_clipping(data, count, 'C');
 
-    printf("Clipping count A: %d\n", clipA);
-    printf("Clipping count B: %d\n", clipB);
-    printf("Clipping count C: %d\n", clipC);
-
     int tolA = within_tolerance(rmsA);
     int tolB = within_tolerance(rmsB);
     int tolC = within_tolerance(rmsC);
-
-    printf("Tolerance A: %d\n", tolA);
-    printf("Tolerance B: %d\n", tolB);
-    printf("Tolerance C: %d\n", tolC);
 
     write_results("results.txt",
                   rmsA, rmsB, rmsC,
